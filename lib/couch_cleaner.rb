3ee0145a54ed2af7a42f30c05576cfcacf8212ca
Xavier Lange <xrlange@gmail.com>
2012-03-22 13:54:46 -0700

    Now with multibyte regex

--- lib/couch_cleaner.rb
+++ lib/couch_cleaner.rb
@@ -5,7 +5,10 @@ require 'bundler'
 Bundler.require
 
 require 'iconv'
-require_relative 'diff.rb'
+require 'diff/lcs'
+
+require 'couch_cleaner/database'
+require 'couch_cleaner/version'
 
 # puts resp.type_params
 # def charset_to_ruby_encoding(charset)
@@ -13,25 +16,29 @@ require_relative 'diff.rb'
 # end
 # puts charset_to_ruby_encoding(resp.type_params["charset"])
 
-require 'couch_cleaner/database'
-
 module CouchCleaner
-  class Cleaner
-    def self.conv(db,from_encoding = "US-ASCII",to_encoding = "UTF-8")
-      doc_ids = db.all_doc_ids
-      diffs = []
-      doc_ids.each_slice(100) do |ids_subset|
-        # print "Getting docs in bulk... "
-        docs = db.get_bulk_doc_resp(ids_subset)
-        # puts "Done!"
-        # print "Converting with iconv... "
-        iconv_body = Iconv.conv(from_encoding,to_encoding,docs.body)
-        # puts "Done!"
-        # print "Diffing... "
-        diffs << Differ.diff_by_line(docs.body,iconv_body)
-        # puts "Done!"
-      end
-      diffs
-    end
+  is_multibyte = Regexp.new(/\\u*\B/)
+  def self.contains_escaped_multibyte_seq?(raw_doc)
+    is_multibyte.match(raw_doc)
+  end
+  def self.conv(raw_doc,from_encoding = "LATIN1//TRANSLIT//IGNORE",to_encoding = "UTF-8")
+    Iconv.conv(from_encoding,to_encoding,raw_doc)
+  end
+  def self.conv_db(db)
+    # doc_ids = db.all_doc_ids
+    # diffs = []
+    # doc_ids.each_slice(100) do |ids_subset|
+    #   # print "Getting docs in bulk... "
+    #   docs = db.get_bulk_doc_resp(ids_subset)
+    #   ascii_body = docs.body
+    #   # puts "Done!"
+    #   # print "Converting with iconv... "
+    #   iconv_body = Iconv.conv(from_encoding,to_encoding,ascii_body)
+    #   # puts "Done!"
+    #   # print "Diffing... "
+    #   diffs << Diff::LCS.diff(ascii_body,iconv_body)
+    #   # puts "Done!"
+    # end
+    # diffs
   end
 end
